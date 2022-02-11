1453ca93cbc0fcb6e888800dfe76acdb11d031ff
Jose <jloai009@fiu.edu>
2022-02-10 21:18:38 -0500

    Define Button and Input styled components

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ import {
   useHistory,
 } from "react-router-dom"
 
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+
 const Home = () => (
   <div> 
     <h2>TKTL notes app</h2> 
